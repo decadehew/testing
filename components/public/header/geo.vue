d4abeae73175c573cb8bdda7a95715055dccbf0a
decadehew <yong_323@hotmail.com>
2022-02-17 21:31:17 +0800

    chore: update

---
+++ components/public/header/geo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" /> 台中市
+    <nuxt-link
+      class="change-city"
+      to="/change-city"
+    >
+      切換城市
+    </nuxt-link>
+    [台北 台中 台南]
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="css">
+</style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6">
+      <geo />
+    </el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+<script>
+import Geo from './geo';
+
+export default {
+  name: 'AppHeader',
+  components: {
+    Geo
+  }
+}
+</script>
+<style lang="scss">
+ @import "@/assets/css/public/layout.scss";
+</style>
